3fea149cea6546029a1b88ff19b001afd44226e0
SabbirIqbalFarhan <sabbiriqbalfarhan43@gmail.com>
2026-05-21 21:18:07 +0600

    Add validation to Module model

--- Exam_Test/Models/Module.cs
+++ Exam_Test/Models/Module.cs
@@ -1,8 +1,13 @@
-﻿namespace Exam_Test.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Exam_Test.Models
 {
     public class Module
     {
         public int Id { get; set; }
-        public string? Name { get; set; } // Module 1, 2, 3
+
+        [Required]
+        [StringLength(100)]
+        public string? Name { get; set; }
     }
 }
